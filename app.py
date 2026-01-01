55b2a2b700c49e1129df42a05a09ad8245ea4780
th30bl4ck <theoblack023@gmail.com>
2026-01-01 15:30:45 +0000

    1

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return send_from_directory(".", "index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
